6c4398b7b04251d3d5ee993a5d810b8e1e0ce30b
Adamiomn <adam.pescoller@gmail.com>
2022-01-21 11:31:00 +0100

    refactored most functions

--- 08_NaiveMap/Solutions/NaiveMap.c
+++ 08_NaiveMap/Solutions/NaiveMap.c
@@ -28,6 +28,43 @@ void ReallocateMapMemory(NaiveMap * const map)
 	}
 }
 
+int GetKeyIndex(const NaiveMap * const map, const char * key)
+{
+	for (size_t i = 0; i < map->size; ++i)
+	{
+		if (strcmp(map->mapEntries[i].key, key) == 0)
+		{
+			return (int)i;
+		}
+	}
+	return -1;
+}
+
+int GetValueIndex(const NaiveMap * const map, const void * const value)
+{
+	for (size_t i = 0; i < map->size; ++i)
+	{
+		if (map->mapEntries[i].value == value)
+		{
+			return (int)i;
+		}
+	}
+	return -1;
+}
+
+void AddKeyValuePair(NaiveMap * const map, const char * key, void * const value)
+{
+	map->mapEntries[map->size - 1].key = malloc(sizeof(char) * (strlen(key) + 1));
+	if (!map->mapEntries[map->size - 1].key)
+	{
+		fprintf(stderr, "ERROR! Could not allocate memory for key of key-value pair.");
+		exit(EXIT_FAILURE);
+	}
+
+	strcpy(map->mapEntries[map->size - 1].key, key);
+	map->mapEntries[map->size - 1].value = value;
+}
+
 void RemoveKeyValuePair(NaiveMap * const map, const size_t index)
 {
 	free(map->mapEntries[index].key);
@@ -85,30 +122,19 @@ bool Map_Insert(NaiveMap * const map, const char * key, void * const value, cons
 		return false;
 	}
 
-	for (size_t i = 0; i < map->size; ++i)
+	const int keyIndex = GetKeyIndex(map, key);
+	if (keyIndex != -1)
 	{
-		if (strcmp(key, map->mapEntries[i].key) == 0)
+		if (allowReassignment)
 		{
-			if (allowReassignment)
-			{
-				map->mapEntries[i].value = value;
-			}
-			return allowReassignment;
+			map->mapEntries[keyIndex].value = value;
 		}
+		return allowReassignment;
 	}
 
 	++map->size;
 	ReallocateMapMemory(map);	// reallocating memory every time we add an element is wasteful but is done here for simplicity, geometric resizing would be preferred
-
-	map->mapEntries[map->size - 1].key = malloc(sizeof(char) * (strlen(key) + 1));
-	if (!map->mapEntries[map->size - 1].key)
-	{
-		fprintf(stderr, "ERROR! Could not allocate memory for key of key-value pair.");
-		exit(EXIT_FAILURE);
-	}
-
-	strcpy(map->mapEntries[map->size - 1].key, key);
-	map->mapEntries[map->size - 1].value = value;
+	AddKeyValuePair(map, key, value);
 
 	return true;
 }
@@ -120,12 +146,10 @@ void * Map_GetValue(NaiveMap * const map, const char * key)
 		return NULL;
 	}
 
-	for (size_t i = 0; i < map->size; ++i)
+	const int keyIndex = GetKeyIndex(map, key);
+	if (keyIndex != -1)
 	{
-		if (strcmp(key, map->mapEntries[i].key) == 0)
-		{
-			return map->mapEntries[i].value;
-		}
+		return map->mapEntries[keyIndex].value;
 	}
 	return NULL;
 }
@@ -137,34 +161,33 @@ bool Map_RemoveByKey(NaiveMap * const map, const char * key)
 		return false;
 	}
 
-	for (size_t i = 0; i < map->size; ++i)
-	{
-		if (strcmp(key, map->mapEntries[i].key) == 0)
-		{
-			RemoveKeyValuePair(map, i);
-			return true;
-		}
-	}
-	return false;
-}
-size_t Map_RemoveByValue(NaiveMap * const map, const void * value)
-{
-	if (!map)
+	const int keyIndex = GetKeyIndex(map, key);
+	if (keyIndex == -1)
 	{
 		return false;
 	}
 
-	size_t removedItemCount = 0;
-	for (size_t i = 0; i < map->size; ++i)
+	RemoveKeyValuePair(map, (size_t)keyIndex);
+	return true;
+}
+
+size_t Map_RemoveByValue(NaiveMap * const map, const void * value)
+{
+	if (!map)
 	{
-		if (value == map->mapEntries[i].value)
-		{
-			RemoveKeyValuePair(map, i);
-			++removedItemCount;
-		}
+		return 0;
+	}
+
+	size_t removedItemCount = 0;
+	int valueIndex;
+	while((valueIndex = GetValueIndex(map, value)) != -1)
+	{
+		RemoveKeyValuePair(map, (size_t)valueIndex);
+		++removedItemCount;
 	}
 	return removedItemCount;
 }
+
 size_t Map_MatchRemoveByKeyPredicate(NaiveMap * const map, const char * key, bool (*predicate)(const char *, const char *))
 {
 	if (!map)
@@ -183,6 +206,7 @@ size_t Map_MatchRemoveByKeyPredicate(NaiveMap * const map, const char * key, boo
 	}
 	return removedItemCount;
 }
+
 size_t Map_MatchRemoveByPredicate(NaiveMap * const map, bool (*predicate)(const char *))
 {
 	if (!map)
@@ -201,6 +225,7 @@ size_t Map_MatchRemoveByPredicate(NaiveMap * const map, bool (*predicate)(const 
 	}
 	return removedItemCount;
 }
+
 bool Map_ContainsKey(const NaiveMap * const map, const char * key)
 {
 	if (!map)
@@ -208,15 +233,9 @@ bool Map_ContainsKey(const NaiveMap * const map, const char * key)
 		return false;
 	}
 
-	for (size_t i = 0; i < map->size; ++i)
-	{
-		if (strcmp(key, map->mapEntries[i].key) == 0)
-		{
-			return true;
-		}
-	}
-	return false;
+	return GetKeyIndex(map, key) != -1;
 }
+
 bool Map_ContainsValue(const NaiveMap * const map, void * const value)
 {
 	if (!map)
@@ -224,12 +243,5 @@ bool Map_ContainsValue(const NaiveMap * const map, void * const value)
 		return false;
 	}
 
-	for (size_t i = 0; i < map->size; ++i)
-	{
-		if (value == map->mapEntries[i].value)
-		{
-			return true;
-		}
-	}
-	return false;
+	return GetValueIndex(map, value) != -1;
 }
