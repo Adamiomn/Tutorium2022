358e8e92c41b24c260e5b94334ef993108723590
Adamiomn <adam.pescoller@gmail.com>
2022-06-24 00:15:38 +0200

    added exercises

---
+++ 9999_KlausurFragen/ControlStatements/ControlStatements_004.c
@@ -0,0 +1,11 @@
+do
+{
+	printf("Hello loop!\n");
+	continue;
+} while (0)
+
+// Wie oft wird "Hello loop!" ausgegeben? Wähle die richtige Aussage aus.
+//		a) ☐ Gar nie
+//		b) ☐ Ein Mal
+//		c) ☐ Zwei Mal
+//		d) ☐ Unendlich Mal (Endlosschleife)
---
+++ 9999_KlausurFragen/ControlStatements/SOL_ControlStatements_004.c
@@ -0,0 +1,10 @@
+do
+{
+	printf("Hello loop!\n");
+	continue;
+} while (0)
+
+// Wie oft wird "Hello loop!" ausgegeben? Wähle die richtige Aussage aus.
+//		b) Ein Mal
+// Erklärung:
+//		b) Zuerst wird printf ausgegeben, dann wird mit continue zur Schleifenbedingung gesprungen und die Schleife wird abgebrochen (continue springt nicht zum "Anfang" der Schleife).
