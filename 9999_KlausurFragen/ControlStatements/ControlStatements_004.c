0a57745c0ecd66982b98c3a9803226ac7b64f729
Adamiomn <adam.pescoller@gmail.com>
2022-06-25 17:14:02 +0200

    small adjustments

--- 9999_KlausurFragen/ControlStatements/ControlStatements_004.c
+++ 9999_KlausurFragen/ControlStatements/ControlStatements_004.c
@@ -5,7 +5,7 @@ do
 } while (0)
 
 // Wie oft wird "Hello loop!" ausgegeben? Wähle die richtige Aussage aus.
-//		a) ☐ Gar nie
-//		b) ☐ Ein Mal
-//		c) ☐ Zwei Mal
-//		d) ☐ Unendlich Mal (Endlosschleife)
+//		a) Gar nie
+//		b) Ein Mal
+//		c) Zwei Mal
+//		d) Unendlich Mal (Endlosschleife)
